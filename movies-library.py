df61c85db8696b541b21eff0f6f2cc29f0bf1f24
Lukasz Zytko <zytko.lukasz@gmail.com>
2021-12-20 21:35:45 +0100

    Add functions

--- movies-library.py
+++ movies-library.py
@@ -33,6 +33,7 @@ movie2 = Movies(title = "Nietykalni", year = "2011", genre = "Komedia")
 movie3 = Movies(title = "Zielona Mila", year = "1999", genre = "Dramat")
 movie4 = Movies(title = "Ojciec Chrzestny", year = "1972", genre = "Gangsterski")
 movie5 = Movies(title = "Dwunastu Gniewnych Ludzi", year = "1957", genre = "Dramat Sądowy")
+
 series1 = TvSeries(title = "Gra o Tron", year = "2011", genre = "Fantasy", episode=1, season=1)
 series2 = TvSeries(title = "Czarnobyl", year = "2019", genre = "Dramat", episode=1, season=1)
 series3 = TvSeries(title = "Breaking Bad", year = "2008", genre = "Kryminał", episode=1, season=1)
@@ -44,20 +45,16 @@ series_library = []
 movies_library = []
 
 def get_series():
-    print("--- Lista seriali: ---")
-    for item in library:
+    print("\n--- Lista seriali: ---")
+    for item in sorted(library, key = lambda item: item.title):
         if isinstance(item, TvSeries):
-            series_library.append(item)
-    for item in sorted(series_library, key = lambda item: item.title):
-        print(item)
+            print(item)
 
 def get_movies():
-    print("--- Lista filmów: ---")
-    for item in library:
+    print("\n--- Lista filmów: ---")
+    for item in sorted(library, key = lambda item: item.title):
         if not isinstance(item, TvSeries):
-            movies_library.append(item)
-    for item in sorted(movies_library, key = lambda item: item.title):
-        print(item)
+            print(item)
 
 def search():
     title = input("Jakiego filmu szukasz: ")
@@ -65,15 +62,38 @@ def search():
         if item.title.lower() == title.lower():
             print(item)
 
-#def generate_views_mulitplier():
+def generate_views_mulitplier(func):
+    def wrapper(*args, **kwargs):
+        for i in range(10):
+            func(*args, **kwargs)
+    return wrapper
 
-
+@generate_views_mulitplier
 def generate_views(seq=library):
     item = rand.choice(seq)
     item.played = rand.randint(0, 101)
 
+def top_titles(count=3,type=None):  
+    if type == None:
+        print(f"\n--- Lista TOP {count} najlepszych: ---")
+        for item in sorted(library, key = lambda item: item.played, reverse=True)[:count]:
+            print(item)  
+    elif type == "m":
+        print(f"\n--- Lista TOP {count} filmów: ---")
+        for item in sorted(library, key = lambda item: item.played, reverse=True)[:count]:
+            if not isinstance(item, TvSeries):
+                print(item)
+    elif type == "s":
+        print(f"\n--- Lista TOP {count} seriali: ---")
+        for item in sorted(library, key = lambda item: item.played, reverse=True)[:count]:
+            if isinstance(item, TvSeries):     
+                print(item)
 
+def add_series(stitle, syear, sgenre, season_no, episodes_no):
+    for i in range(1, episodes_no+1):
+        library.append(TvSeries(title = stitle, year = syear, genre = sgenre, episode = i, season = season_no))
 
+add_series("Sherlock", 2010, "kryminał", 2, 5)
 generate_views()
 #movie2.play()
 #movie1.play(12)
@@ -81,6 +101,7 @@ generate_views()
 #series2.play(14)
 get_series()
 get_movies()
+top_titles(type='s')
 #search()
 #print([rand.randint(0, 101), rand.randint(0, 101), rand.randint(0, 101)])
 #print(rand.choice(library))
