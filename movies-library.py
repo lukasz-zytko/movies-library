0064cd55c4f78ef5461dfdb5ad1a4752f5b8afd4
Lukasz Zytko <zytko.lukasz@gmail.com>
2021-12-20 20:21:33 +0100

    Add player generator

--- movies-library.py
+++ movies-library.py
@@ -1,3 +1,6 @@
+from random import Random
+rand = Random()
+
 class Movies:
     def __init__(self, title, year, genre, played=0):
         self.title = title
@@ -6,7 +9,7 @@ class Movies:
         self.played = played
     
     def __str__(self) -> str:
-        return f"{self.title} ({self.year}) - obejrzany: {self.played} razy"
+        return f"{self.title} ({self.year}) - liczba odtworzeń: {self.played}"
     
     def play(self,step=1):
         self.played += step
@@ -23,21 +26,25 @@ class TvSeries(Movies):
         self.season = season
 
     def __str__(self) -> str:
-        return f"{self.title} S{self.season}E{self.episode} - obejrzany: {self.played} razy"
-
+        return f"{self.title} S{self.season}E{self.episode} - liczba odtworzeń: {self.played}"
 
 movie1 = Movies(title = "Skazani na Shawshank", year = "1994", genre = "Dramat")
 movie2 = Movies(title = "Nietykalni", year = "2011", genre = "Komedia")
+movie3 = Movies(title = "Zielona Mila", year = "1999", genre = "Dramat")
+movie4 = Movies(title = "Ojciec Chrzestny", year = "1972", genre = "Gangsterski")
+movie5 = Movies(title = "Dwunastu Gniewnych Ludzi", year = "1957", genre = "Dramat Sądowy")
 series1 = TvSeries(title = "Gra o Tron", year = "2011", genre = "Fantasy", episode=1, season=1)
 series2 = TvSeries(title = "Czarnobyl", year = "2019", genre = "Dramat", episode=1, season=1)
+series3 = TvSeries(title = "Breaking Bad", year = "2008", genre = "Kryminał", episode=1, season=1)
+series4 = TvSeries(title = "Kompania Braci", year = "2001", genre = "Wojenny", episode=1, season=1)
+series5 = TvSeries(title = "Biuro", year = "2005", genre = "komedia", episode=1, season=1)
 
-
-library = [movie1, movie2, series1, series2]
+library = [movie1, movie2, movie3, movie4, movie5, series1, series2, series3, series4, series5]
 series_library = []
 movies_library = []
 
 def get_series():
-    print("Lista seriali:")
+    print("--- Lista seriali: ---")
     for item in library:
         if isinstance(item, TvSeries):
             series_library.append(item)
@@ -45,7 +52,7 @@ def get_series():
         print(item)
 
 def get_movies():
-    print("Lista filmów:")
+    print("--- Lista filmów: ---")
     for item in library:
         if not isinstance(item, TvSeries):
             movies_library.append(item)
@@ -58,15 +65,27 @@ def search():
         if item.title.lower() == title.lower():
             print(item)
 
+#def generate_views_mulitplier():
 
-movie2.play()
-movie1.play(12)
-series1.play(98)
-series2.play(14)
+
+def generate_views(seq=library):
+    item = rand.choice(seq)
+    item.played = rand.randint(0, 101)
+
+
+
+generate_views()
+#movie2.play()
+#movie1.play(12)
+#series1.play(98)
+#series2.play(14)
 get_series()
 get_movies()
-search()
+#search()
+#print([rand.randint(0, 101), rand.randint(0, 101), rand.randint(0, 101)])
+#print(rand.choice(library))
 
+#print(dir(rand))
 
 #for item in library:
 #    if isinstance(item, TvSeries):
